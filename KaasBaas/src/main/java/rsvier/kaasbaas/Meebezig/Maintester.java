4c17ad53a1795256f93f8e4a7685deb4f1183725
FRANK <lorus1@live.nl>
2017-01-26 14:00:22 +0100

    create/update problemen.

--- KaasBaas/src/main/java/rsvier/kaasbaas/Meebezig/Maintester.java
+++ KaasBaas/src/main/java/rsvier/kaasbaas/Meebezig/Maintester.java
@@ -4,22 +4,37 @@ package opdracht6test;
 
 import java.sql.*;
 
+import java.util.*;
+
+import opdracht6test.KlantInterface;
+
+
+
 
 
 /**
  *
  * @author Frank
  */
-public class Maintester extends Klant {
+public class Maintester  {
     
     SQLConnection connectie = new SQLConnection();
     KlantInterface KlantDAO = new KlantDAOinterfaceImpl(connectie.getSQLConnection());
     
     
-    public static void Main(String[] args){
+    public static void main(String[] args){
+        
+        Klant testklant = new Klant();
+     
+        System.out.println("Maak eerst een klant");
+      
+
+        testklant.setALL(2, "Henk", "de", "Groot", 0612312311, 1, "A", "1234 AA", "1234 BB", 2, "B", "Nederland");
         
         
+   
         
+  //update(testklant);
         
         
     }
